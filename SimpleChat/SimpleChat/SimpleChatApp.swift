69c6aff7a4b2a9e7a2dc266a3a58166cba2fbf4f
znak99 <xhahekcldhkznak0113@gmail.com>
2023-05-06 01:02:10 +0900

    Init Xcode

---
+++ SimpleChat/SimpleChat/SimpleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatApp.swift
+//  SimpleChat
+//
+//  Created by seungwoo on 2023/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
